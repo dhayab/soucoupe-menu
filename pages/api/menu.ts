a86097a65734927a92e2005f70ba3a53c6e7e127
Dhaya Benmessaoud <154633+dhayab@users.noreply.github.com>
2020-02-29 22:07:46 +0100

    feat: generate homepage with nextjs

--- pages/api/menu.ts
+++ pages/api/menu.ts
@@ -6,5 +6,8 @@ export default async (_req: NowRequest, res: NowResponse) => {
 	const { PAGE_URL, PAGE_ARTICLES } = process.env;
 	const articles = await scrape(PAGE_URL!, parseInt(PAGE_ARTICLES!));
 
+	// See: https://zeit.co/docs/v2/serverless-functions/edge-caching?query=cache#stale-while-revalidate
+	res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
+
 	res.json(articles);
 };
---
+++ pages/index.tsx
@@ -0,0 +1,41 @@
+import './index.scss';
+
+import { formatDistanceToNow } from 'date-fns';
+import { fr } from 'date-fns/locale';
+import fetch from 'isomorphic-unfetch';
+import { NextPage } from 'next';
+import Head from 'next/head';
+import * as React from 'react';
+
+type MenuApi = {
+	message: string,
+	created_time: Date,
+};
+
+const Home: NextPage<MenuApi> = ({ message, created_time }) => {
+	const date = new Date(created_time);
+	const formattedDate = date.toLocaleString('fr', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric' });
+	const relativeDate = formatDistanceToNow(date, { addSuffix: true, locale: fr });
+	return (
+		<main>
+			<Head>
+				<title>Café Soucoupe • Menu du jour</title>
+			</Head>
+			<header>
+				<img src="/logo.jpg" alt="Café Soucoupe" title="Café Soucoupe" />
+			</header>
+			<p className="menu">{message}</p>
+			<footer>
+				<time dateTime={date.toISOString()} title={formattedDate}>{relativeDate}</time>
+			</footer>
+		</main>
+	)
+};
+
+Home.getInitialProps = async () => {
+	const res = await fetch('https://soucoupe.now.sh/api/menu');
+	const [menu] = await res.json();
+	return menu;
+}
+
+export default Home;
